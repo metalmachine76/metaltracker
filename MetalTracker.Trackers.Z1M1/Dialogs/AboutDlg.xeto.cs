3f49c7eeff183b7b0a376a3dca7a385bf07a3d7b
metal <metal>
2022-12-04 11:17:31 -0700

    fixed repo link

--- MetalTracker.Trackers.Z1M1/Dialogs/AboutDlg.xeto.cs
+++ MetalTracker.Trackers.Z1M1/Dialogs/AboutDlg.xeto.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Reflection;
 using Eto.Forms;
 using Eto.Serialization.Xaml;
@@ -18,6 +19,11 @@ namespace MetalTracker.Trackers.Z1M1.Dialogs
 			var assyVersion = Assembly.GetExecutingAssembly().GetName().Version;
 			string versionString = $"{assyVersion.Major}.{assyVersion.Minor}.{assyVersion.Build}";
 			this.FindChild<Label>("labelVersion").Text = $"Version {versionString} ({platform.ID})";
+		}
+
+		protected void HandleLinkClick(object sender, EventArgs e)
+		{
+			Process.Start(new ProcessStartInfo { UseShellExecute = true, FileName = "https://github.com/metalmachine76/metaltracker" });
 		}
 	}
 }
